2ea38b4476f701630db46c28e5a6395a3fee668f
Daniel Kagan <danielk@wix.com>
2025-04-20 15:46:26 +0300

    Update rb

--- glogm.rb
+++ glogm.rb
@@ -9,20 +9,10 @@ class Glogm < Formula
   depends_on "git-delta"
 
   def install
-    # Ensure the `glogm` script has a proper shebang
-    libexec.install Dir["glogm"]
-    chmod "+x", libexec/"glogm"
-
-    # Create a wrapper script in `bin`
-    bin.write_exec_script libexec/"glogm"
+    bin.install "glogm"
   end
 
   test do
-    # Verify that the `glogm` script exists and is executable
-    assert_predicate bin/"glogm", :exist?, "glogm binary should exist"
-    assert_predicate bin/"glogm", :executable?, "glogm binary should be executable"
-
-    # Run a basic test to confirm the script works
-    system "#{bin}/glogm", "--help" # Replace with an actual command if applicable
+    assert_predicate bin/"glogm", :executable?
   end
 end
